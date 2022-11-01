f3602719442c2ab7c6d915f24baa09e4508d5980
Faiz <faiz@Laptop-Faiz.local>
2022-11-02 06:21:04 +0800

    add splashscreen/loadingscreen

---
+++ lib/splash_screen.dart
@@ -0,0 +1,70 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:gohomestay/home.dart';
+
+class LoadingScreen extends StatefulWidget {
+  LoadingScreen({Key? key}) : super(key: key);
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => HomeScreen())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Color(0xFF007991),
+              Color(0xFF78ffd6),
+            ],
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              children: [
+                Image.asset(
+                  "assets/loading.png",
+                  height: 300.00,
+                  width: 300.00,
+                ),
+                Text(
+                  "Go Homestay",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                  ),
+                )
+              ],
+            ),
+            CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            )
+          ],
+        ),
+      ), //end of body container
+    ); //end of return scaffold
+  } //end of widget build
+}//end of class _LoadingScreenState
+
+
