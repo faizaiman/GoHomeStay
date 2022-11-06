4a5235bf5fef577834439590731ef0e2346a7492
Faizznddn <mohamaddfaizaiman@gmail.com>
2022-11-06 17:30:23 +0800

    Fix bugs
    FIx bugs ahamad yang noob

--- lib/home.dart
+++ lib/home.dart
@@ -1,142 +1,98 @@
-
 import 'package:flutter/material.dart';
 import 'package:gohomestay/homestaydetail.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'homestay.dart';
 import 'homestaydetail.dart';
 
-class HomePage extends StatefulWidget
-{
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
-  @override 
-  State<HomePage> createState() =>_HomePageState();
+  @override
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>
-{
-
-  Widget buildHomeStay(Homestay homestay)
-  {
-    return Card
-    (
-      elevation: 1.5,
-      shape: RoundedRectangleBorder
-      (
-        borderRadius: BorderRadius.circular
-        (
-          5.0
-        )
-      ),
-      child: Padding
-      (
-      padding: const EdgeInsets.all
-      (
-        10.0
-      ),
-      child: Column
-      (
-        children:<Widget>
-        [
-        Image(image:AssetImage(homestay.thumbnailUrl)),
-        const SizedBox(height: 10.0, width: 15.0,),
-        Text
-        (
-          homestay.listingName,
-          style:const TextStyle
-          (
-            fontSize: 20.0,
-            fontWeight: FontWeight.w700, 
-            fontFamily: 'GoogleFonts.lato()',
-          ),
+class _HomePageState extends State<HomePage> {
+  Widget buildHomeStay(Homestay homestay) {
+    return Center(
+        child: Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        elevation: 4,
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+              ),
+              child: Image(
+                image: AssetImage(homestay.thumbnailUrl),
+                height: 250,
+                width: MediaQuery.of(context).size.width,
+                fit: BoxFit.fill,
+              ),
+            ),
+            ListTile(
+              title: Text(
+                homestay.listingName,
+                style: TextStyle(fontSize: 16),
+              ),
+              trailing: Text(
+                homestay.listingAddress,
+                style: TextStyle(fontSize: 10),
+              ),
+            )
+          ],
         ),
-        Text
-        (
-          homestay.listingAddress,
-          style:const TextStyle
-          (
-            fontSize: 14.0,
-            fontWeight: FontWeight.w300, 
-            fontFamily: 'GoogleFonts.lato()',
-          ),
-        ),
-        Text
-        (
-          'RM ${homestay.listingPrice} Per Night',
-          style:const TextStyle
-          (
-            fontSize: 14.0,
-            fontWeight: FontWeight.w300, 
-            fontFamily: 'GoogleFonts.lato()',
-          ),
-        ),
-      ],
       ),
-      ),
-      );
+    ));
   }
 
   @override
-  Widget build(BuildContext context)
-  {
-    return Scaffold
-    (
-      appBar: AppBar
-      ( 
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
         centerTitle: true,
-        title: SizedBox
-        (
+        title: SizedBox(
           width: 40,
           child: Image.asset('assets/loading.png'),
         ),
-        bottom: const PreferredSize
-        (
+        bottom: const PreferredSize(
           preferredSize: Size.zero,
-          child: Text
-          (
+          child: Text(
             "Homestay Listing",
-            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 13),
+            style: TextStyle(
+                color: Colors.black, fontWeight: FontWeight.bold, fontSize: 13),
           ),
         ),
         toolbarHeight: 60.2,
         toolbarOpacity: 0.8,
-        shape: const RoundedRectangleBorder
-        (
-          borderRadius: BorderRadius.only
-          (
-            bottomRight: Radius.circular(5),
-            bottomLeft: Radius.circular(5)
-          ),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(5), bottomLeft: Radius.circular(5)),
         ),
         backgroundColor: Colors.white,
       ),
-      
-      body: SafeArea
-      (
-            child: ListView.builder
-            (
-              itemCount: Homestay.listings.length,
-              itemBuilder: (BuildContext context, int index)
-              {
-                return GestureDetector
-                (
-                  child: buildHomeStay(Homestay.listings[index]),
-                  onTap:() 
-                  {
-                    Navigator.push
-                    (
-                      context, MaterialPageRoute
-                      (
-                        builder: (context)
-                        {
-                          return HomeDetail(homestay: Homestay.listings[index]);
-                        }
-                      )
-                    );
-                  },
-                );           
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: Homestay.listings.length,
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return HomeDetail(homestay: Homestay.listings[index]);
+                    },
+                  ),
+                );
               },
-            ),
-        ), 
-      ); 
+              child: buildHomeStay(Homestay.listings[index]),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
--- lib/homestay.dart
+++ lib/homestay.dart
@@ -1,55 +1,35 @@
-class Homestay
-{
-  int listingID;
+class Homestay {
   String listingName;
   double listingPrice;
   String listingAddress;
-  List<String>images;
+  List<String> images;
   String thumbnailUrl;
 
   String discountCode;
-  Homestay(this.listingID, this.listingName, this.listingPrice, this.listingAddress, this.thumbnailUrl, this.images, this.discountCode);
+  Homestay(this.listingName, this.listingPrice, this.listingAddress,
+      this.thumbnailUrl, this.images, this.discountCode);
 
-  static List<Homestay>listings =
-  [
-    Homestay
-    (
-      0,
-      'Duplex @ Mutiara Ville', 
-      200.00, 
-      'Mutiara Ville, Cyberjaya, Selangor',
-      'assets/mutiaraville.jpg',
-      [
+  static List<Homestay> listings = [
+    Homestay(
+        'Duplex @ Mutiara Ville',
+        200.00,
+        'Mutiara Ville, Cyberjaya, Selangor',
         'assets/mutiaraville.jpg',
-        'assets/mutiaraville2.jpeg'
-      ],
-      'ABC123'
-    ),
-    Homestay
-    (
-      1,
-      'Homestay BSP21',
-      400.00,
-      'BSP21, Bandar Saujana Putra, Selangor',
-      'assets/bsp21.jpg',
-      [
-        'assets/bsp21'
-        'assets/bsp212.png',
-      ],
-      'DEF456'
-    ),
-    Homestay
-    (
-      2,
-      'Pent House @ KL Traders Square Residence',
-      400.00,
-      'KL Traders Square Residence, Setapak, Kuala Lumpur',
-      'assets/kl-traders-square.jpg',
-      [
+        ['assets/mutiaraville.jpg', 'assets/mutiaraville2.jpeg'],
+        'ABC123'),
+    Homestay(
+        'Homestay BSP21',
+        400.00,
+        'BSP21, Bandar Saujana Putra, Selangor',
+        'assets/bsp21.jpg',
+        ['assets/bsp21.jpg', 'assets/bsp212.jpeg'],
+        'DEF456'),
+    Homestay(
+        'Pent House @ KLTS',
+        400.00,
+        'KLTS, Setapak, Kuala Lumpur',
         'assets/kl-traders-square.jpg',
-        'assets/kl-traders-square2.jpeg'
-      ],
-      'GHI789'
-    ),
+        ['assets/kl-traders-square.jpg', 'assets/kl-traders-square2.jpeg'],
+        'GHI789'),
   ];
 }
